725bac437bcf0a6c01e4565e9146b73091920b05
movimento-maker <contato@movimentomaker.com.br>
2019-03-28 16:55:01 -0300

    Inclusão do materialize css e controllers

---
+++ DesafioSolution/DesafioWeb/Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using DesafioWeb.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DesafioWeb.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public IEnumerable<PorteEmpresaVM> GetAllPorteEmpresa()
+        {
+            return null;
+        }
+    }
+}
